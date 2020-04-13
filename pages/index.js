c8132bbf07890b5e66a248245197ebf0e27db322
Deleon Reescano <deleon.reescano@ricealumni.net>
2020-04-13 06:48:44 -0500

    dynamic pages with query strings

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,25 @@
+import Link from "next/link";
+
 import Layout from "../components/MyLayout";
 
-export default function Index() {
+const PostLink = props => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+);
+
+export default function Blog() {
   return (
     <div>
       <Layout>
-        <p>Hello Next.js</p>
+        <h1>My Blog</h1>
+        <ul>
+          <PostLink title="Hello Next.js" />
+          <PostLink title="Learning Next.js is awesome" />
+          <PostLink title="Deploy them apps" />
+        </ul>
       </Layout>
     </div>
   );
