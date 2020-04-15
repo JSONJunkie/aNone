8b66b83c9eb2be868603c0d1471c2512d69326fa
drees1992 <deleon.reescano@ricealumni.net>
2020-04-15 08:01:49 -0500

    title

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import Head from "next/head";
 import { ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import theme from "../themes/theme";
+import Navbar from "../components/Navbar";
 
 export default function MyApp(props) {
   const { Component, pageProps } = props;
@@ -19,7 +20,7 @@ export default function MyApp(props) {
   return (
     <React.Fragment>
       <Head>
-        <title>My page</title>
+        <title>localhostin</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
@@ -28,6 +29,7 @@ export default function MyApp(props) {
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
+        <Navbar />
         <Component {...pageProps} />
       </ThemeProvider>
     </React.Fragment>
