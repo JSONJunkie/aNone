134ffeb6bfafa104a53ede1d3296d44f0ce80cba
drees1992 <deleon.reescano@ricealumni.net>
2020-04-15 08:02:53 -0500

    fixed navbar and toolbar for app name. hide on scroll too

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,10 @@
 import { makeStyles } from "@material-ui/core/styles";
 import { Fragment } from "react";
 import AppBar from "@material-ui/core/AppBar";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import Slide from "@material-ui/core/Slide";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -8,21 +12,37 @@ const useStyles = makeStyles(theme => ({
     // height: theme.spacing(5)
     // position: "absolute",
     // top: "0px"
-  },
-  content: {
-    flexGrow: 1,
-    paddingTop: theme.spacing(5)
-    // background: "black"
   }
+  // content: {
+  //   flexGrow: 1,
+  //   paddingTop: theme.spacing(5)
+  // }
 }));
 
-const Navbar = () => {
+function HideOnScroll(props) {
+  const { children, window } = props;
+  const trigger = useScrollTrigger({ target: window ? window() : undefined });
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
+
+const Navbar = props => {
   const classes = useStyles();
 
   return (
     <Fragment>
       <div className={classes.root}>
-        <AppBar position="sticky">Hello</AppBar>
+        <HideOnScroll {...props}>
+          <AppBar position="fixed">
+            <Toolbar>
+              <Typography>aNone</Typography>
+            </Toolbar>
+          </AppBar>
+        </HideOnScroll>
       </div>
     </Fragment>
   );
