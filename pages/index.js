3cec52d17187547a30cf1a4b98ed5f17655073e0
drees1992 <deleon.reescano@ricealumni.net>
2020-04-16 18:33:47 -0500

    new index page old feed

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,46 @@
 import { useRouter } from "next/router";
-import useSWR from "swr";
+// import useSWR from "swr";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import fetch from "isomorphic-unfetch";
 
-function fetcher(url) {
-  return fetch(url).then(r => r.json());
-}
+import CommentCard from "../components/CommentCard";
+import { Fragment } from "react";
 
-export default function Index() {
-  const { query } = useRouter();
-  const { data, error } = useSWR(
-    `/api/randomQuote${query.author ? "?author=" + query.author : ""}`,
-    fetcher
-  );
-  // The following line has optional chaining, added in Next.js v9.1.5,
-  // is the same as `data && data.author`
-  const author = data?.author;
-  let quote = data?.quote;
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex"
+  },
+  content: {
+    flexGrow: 1,
+    paddingTop: theme.spacing(10)
+  }
+}));
 
-  if (!data) quote = "Loading...";
-  if (error) quote = "Failed to fetch the quote.";
+Index.getInitialProps = async function (context) {
+  const dev = process.env.NODE_ENV !== "production";
+
+  const baseUrl = dev
+    ? "http://localhost:3000"
+    : "http://drees1992-anone.herokuapp.com";
+  const res = await fetch(baseUrl + "/api/test");
+  const data = await res.json();
+
+  return { data };
+};
+
+export default function Index(props) {
+  const classes = useStyles();
 
   return (
-    <main className="center">
-      <div className="quote">{quote}</div>
-      {author && <span className="author">- {author}</span>}
-
-      <style jsx>{`
-        main {
-          width: 90%;
-          max-width: 900px;
-          margin: 300px auto;
-          text-align: center;
-        }
-        .quote {
-          font-family: cursive;
-          color: #e243de;
-          font-size: 24px;
-          padding-bottom: 10px;
-        }
-        .author {
-          font-family: sans-serif;
-          color: #559834;
-          font-size: 20px;
-        }
-      `}</style>
-    </main>
+    <Fragment>
+      <div className={classes.root}>
+        <Container className={classes.content}>
+          {props.data.map(comment => (
+            <CommentCard key={comment._id} data={comment} />
+          ))}
+        </Container>
+      </div>
+    </Fragment>
   );
 }
