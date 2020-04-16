9608efd8dcbfcc818c01d2f0044ae1fe03354581
drees1992 <deleon.reescano@ricealumni.net>
2020-04-16 03:58:08 -0500

    production prep

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,11 @@
-import Layout from "../components/MyLayout";
+// import Layout from "../components/MyLayout";
 
-export default function About() {
-  return (
-    <div>
-      <Layout>
-        <p>This is the about page</p>
-      </Layout>
-    </div>
-  );
-}
+// export default function About() {
+//   return (
+//     <div>
+//       <Layout>
+//         <p>This is the about page</p>
+//       </Layout>
+//     </div>
+//   );
+// }
