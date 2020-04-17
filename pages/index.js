6b1c33f12883aebd5af2f0d79dd1dbe427197553
drees1992 <deleon.reescano@ricealumni.net>
2020-04-16 20:02:56 -0500

    textfield for user input

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
+import { Fragment, useState } from "react";
 import { useRouter } from "next/router";
-// import useSWR from "swr";
+import fetch from "isomorphic-unfetch";
+import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-import fetch from "isomorphic-unfetch";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
 
 import CommentCard from "../components/CommentCard";
-import { Fragment } from "react";
+import { send } from "../src/actions/feed";
+import { connect } from "mongoose";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -14,8 +19,63 @@ const useStyles = makeStyles(theme => ({
   content: {
     flexGrow: 1,
     paddingTop: theme.spacing(10)
+  },
+  input: {
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1)
+  },
+  inputButton: {
+    height: "100%"
   }
 }));
+
+// const Index = ({ data, send }) => {
+export default function Index({ data }) {
+  const classes = useStyles();
+
+  const [text, setText] = useState("");
+
+  const handleText = e => {
+    setText(e.target.value);
+  };
+
+  const handleSend = e => {
+    send(text);
+  };
+
+  return (
+    <Fragment>
+      <div className={classes.root}>
+        <Container className={classes.content}>
+          <Grid container className={classes.input}>
+            <Grid item xs={12} sm>
+              <TextField
+                name="input"
+                variant="outlined"
+                placeholder="Say something..."
+                onChange={e => handleText(e)}
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sm="auto">
+              <Button
+                className={classes.inputButton}
+                color="primary"
+                variant="contained"
+                fullWidth
+              >
+                Send!
+              </Button>
+            </Grid>
+          </Grid>
+          {data.map(comment => (
+            <CommentCard key={comment._id} data={comment} />
+          ))}
+        </Container>
+      </div>
+    </Fragment>
+  );
+}
 
 Index.getInitialProps = async function (context) {
   const dev = process.env.NODE_ENV !== "production";
@@ -29,18 +89,10 @@ Index.getInitialProps = async function (context) {
   return { data };
 };
 
-export default function Index(props) {
-  const classes = useStyles();
+Index.propTypes = {
+  send: PropTypes.func.isRequired
+};
 
-  return (
-    <Fragment>
-      <div className={classes.root}>
-        <Container className={classes.content}>
-          {props.data.map(comment => (
-            <CommentCard key={comment._id} data={comment} />
-          ))}
-        </Container>
-      </div>
-    </Fragment>
-  );
-}
+const mapStateToProps = state => ({});
+
+// export default connect(mapStateToProps, { send })(Index);
