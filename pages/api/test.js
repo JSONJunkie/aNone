4e97046df42a2fc7128e1c912017683bc37a4b4e
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-04-24 02:27:26 -0500

    altering test api routes to throw errors for testing

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,32 +1,50 @@
 import nextConnect from "next-connect";
+import Rollbar from "rollbar";
 
 import middleware from "../../database";
+
+const rollbar = new Rollbar({
+  accessToken: process.env.ROLLBAR_SERVER_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
 
 const handler = nextConnect();
 
 handler.use(middleware);
 
 handler.get(async (req, res) => {
-  const { Comments } = req.models;
-  const { connection } = req.connection;
-  const docs = await Comments.find().sort({ date: -1 });
-  res.json(docs);
-  connection.close();
+  // const { Comments } = req.models;
+  // const { connection } = req.connection;
+  // const docs = await Comments.find().sort({ date: -1 });
+  // res.json(docs);
+  // connection.close();
+
+  try {
+    throw new Error("test get server error");
+  } catch (e) {
+    rollbar.error(e);
+  }
 });
 
 handler.post(async (req, res) => {
-  const { comment, author, id, date } = req.body;
-  const { Comments } = req.models;
-  const { connection } = req.connection;
-  const entry = new Comments({
-    comment,
-    author,
-    id,
-    date
-  });
-  await entry.save();
-  res.json(entry);
-  connection.close();
+  // const { comment, author, id, date } = req.body;
+  // const { Comments } = req.models;
+  // const { connection } = req.connection;
+  // const entry = new Comments({
+  //   comment,
+  //   author,
+  //   id,
+  //   date
+  // });
+  // await entry.save();
+  // res.json(entry);
+  // connection.close();
+  try {
+    throw new Error("test post server error");
+  } catch (e) {
+    rollbar.error(e);
+  }
 });
 
 export default handler;
