86681c86ac0ebc63d3c307cb90766ed6379c94cc
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-04-29 19:18:11 -0500

    conditional loading of posts of state/local tabs, circular loader

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,14 @@ import Container from "@material-ui/core/Container";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Grid from "@material-ui/core/Grid";
 
 import { db } from "../services/firebase";
 import CommentInput from "../components/CommentInput";
 import CommentCard from "../components/CommentCard";
 import { storePos, clear } from "../src/actions/feed";
-import { set } from "mongoose";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -22,6 +24,9 @@ const useStyles = makeStyles(theme => ({
   },
   tabs: {
     marginBottom: theme.spacing(1)
+  },
+  loader: {
+    marginTop: theme.spacing(10)
   }
 }));
 
@@ -35,7 +40,8 @@ function Index({ feed: { sent, location }, storePos, clear, rollbar }) {
   const classes = useStyles();
 
   const [posts, setPosts] = useState([]);
-  const [tab, setTab] = useState(0);
+  const [tab, setTab] = useState("all");
+  const [loading, setLoading] = useState(true);
 
   const handlePos = pos => {
     const crd = pos.coords;
@@ -49,11 +55,13 @@ function Index({ feed: { sent, location }, storePos, clear, rollbar }) {
 
   const handleChange = (event, newValue) => {
     setTab(prev => newValue);
+    setLoading(prev => true);
   };
 
   useEffect(() => {
     if (location) {
-      setTab(prev => 2);
+      setTab(prev => "local");
+      setLoading(prev => true);
     }
   }, [location]);
 
@@ -69,19 +77,58 @@ function Index({ feed: { sent, location }, storePos, clear, rollbar }) {
 
   useEffect(() => {
     try {
-      db.ref("comments")
-        .orderByChild("timestamp")
-        .on("value", snapshot => {
-          let chats = [];
-          snapshot.forEach(snap => {
-            chats.push(snap.val());
+      if (tab === "all") {
+        db.ref("comments")
+          .orderByChild("timestamp")
+          .on("value", snapshot => {
+            let chats = [];
+            snapshot.forEach(snap => {
+              chats.push(snap.val());
+            });
+            setPosts(prev => chats.reverse());
           });
-          setPosts(prev => chats.reverse());
-        });
+        setLoading(prev => false);
+      }
+      if (tab === "state") {
+        if (!location) {
+          setPosts(prev => []);
+          setLoading(prev => false);
+        }
+        if (location) {
+          db.ref("comments")
+            .orderByChild("timestamp")
+            .on("value", snapshot => {
+              let chats = [];
+              snapshot.forEach(snap => {
+                chats.push(snap.val());
+              });
+              setPosts(prev => chats.reverse());
+            });
+          setLoading(prev => false);
+        }
+      }
+      if (tab === "local") {
+        if (!location) {
+          setPosts(prev => []);
+          setLoading(prev => false);
+        }
+        if (location) {
+          db.ref("comments")
+            .orderByChild("timestamp")
+            .on("value", snapshot => {
+              let chats = [];
+              snapshot.forEach(snap => {
+                chats.push(snap.val());
+              });
+              setPosts(prev => chats.reverse());
+            });
+          setLoading(prev => false);
+        }
+      }
     } catch (e) {
       rollbar.error(e);
     }
-  }, []);
+  }, [tab]);
 
   return (
     <Fragment>
@@ -97,16 +144,32 @@ function Index({ feed: { sent, location }, storePos, clear, rollbar }) {
               variant="fullWidth"
               centered
             >
-              <Tab label="All" />
-              <Tab label="State" />
-              <Tab label="Local" />
+              <Tab label="All" value="all" />
+              <Tab label="State" value="state" />
+              <Tab label="Local" value="local" />
             </Tabs>
           </Paper>
           <CommentInput rollbar={rollbar} />
-          {posts &&
-            posts.map(comment => (
-              <CommentCard key={comment.id} data={comment} />
-            ))}
+          {loading ? (
+            <Grid container justify="center" alignItems="center">
+              <CircularProgress className={classes.loader} />
+            </Grid>
+          ) : (
+            <Fragment>
+              {posts.length > 0 ? (
+                posts.map(comment => (
+                  <CommentCard key={comment.id} data={comment} />
+                ))
+              ) : (
+                <Grid container justify="center" alignItems="center">
+                  <Typography>
+                    Not able to retrieve {tab} posts. Please be sure to enable
+                    location for your browser/device.
+                  </Typography>
+                </Grid>
+              )}
+            </Fragment>
+          )}
         </Container>
       </div>
     </Fragment>
