edeb1a402d46e7bd4118379e97a19106d94427f1
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-07-02 08:14:49 -0500

    forgot to prefix the base path environment variable to axios request for redux to work properly in last commit

--- src/actions/feed.js
+++ src/actions/feed.js
@@ -5,13 +5,13 @@ import { SEND, STORE_POS, ERROR, GEO_FAIL, CLEAR_ERROR } from "./types";
 
 const names = ["dog", "horse", "pig", "bird", "cat"];
 
-export const errAlert = ({ message }) => async dispatch => {
+export const errAlert = ({ message }) => async (dispatch) => {
   try {
     dispatch({
       type: ERROR,
       payload: {
-        message
-      }
+        message,
+      },
     });
   } catch (e) {
     rollbar.error(e);
@@ -19,25 +19,29 @@ export const errAlert = ({ message }) => async dispatch => {
       type: ERROR,
       payload: {
         name: e.name,
-        message: e.message
-      }
+        message: e.message,
+      },
     });
   }
 };
 
-export const storePos = ({ crd, rollbar }) => async dispatch => {
+export const storePos = ({ crd, rollbar }) => async (dispatch) => {
   try {
     const { latitude, longitude } = crd;
 
     const res = await axios.get(
-      "/api/test?latitude=" + latitude + "&longitude=" + longitude
+      process.env.BASE_PATH +
+        "/api/test?latitude=" +
+        latitude +
+        "&longitude=" +
+        longitude
     );
 
     const { userCity, userState } = res.data;
 
     dispatch({
       type: STORE_POS,
-      payload: { lat: latitude, long: longitude, userCity, userState }
+      payload: { lat: latitude, long: longitude, userCity, userState },
     });
   } catch (e) {
     if (e.response) {
@@ -47,8 +51,8 @@ export const storePos = ({ crd, rollbar }) => async dispatch => {
           type: ERROR,
           payload: {
             name: errors.namrrors,
-            message: errors.message
-          }
+            message: errors.message,
+          },
         });
       }
     }
@@ -57,16 +61,16 @@ export const storePos = ({ crd, rollbar }) => async dispatch => {
       type: ERROR,
       payload: {
         name: e.name,
-        message: e.message
-      }
+        message: e.message,
+      },
     });
   }
 };
 
-export const geoFail = ({ rollbar }) => async dispatch => {
+export const geoFail = ({ rollbar }) => async (dispatch) => {
   try {
     dispatch({
-      type: GEO_FAIL
+      type: GEO_FAIL,
     });
   } catch (e) {
     rollbar.error(e);
@@ -74,16 +78,16 @@ export const geoFail = ({ rollbar }) => async dispatch => {
       type: ERROR,
       payload: {
         name: e.name,
-        message: e.message
-      }
+        message: e.message,
+      },
     });
   }
 };
 
-export const clear = ({ rollbar }) => async dispatch => {
+export const clear = ({ rollbar }) => async (dispatch) => {
   try {
     dispatch({
-      type: CLEAR_ERROR
+      type: CLEAR_ERROR,
     });
   } catch (e) {
     rollbar.error(e);
@@ -91,18 +95,15 @@ export const clear = ({ rollbar }) => async dispatch => {
       type: ERROR,
       payload: {
         name: e.name,
-        message: e.message
-      }
+        message: e.message,
+      },
     });
   }
 };
 
-export const send = ({
-  text,
-  userCity,
-  userState,
-  rollbar
-}) => async dispatch => {
+export const send = ({ text, userCity, userState, rollbar }) => async (
+  dispatch
+) => {
   try {
     const body = {
       comment: text,
@@ -110,14 +111,14 @@ export const send = ({
       userState,
       author: names[Math.floor(Math.random() * 5)],
       id: uuidv4(),
-      timestamp: new Date()
+      timestamp: new Date(),
     };
 
-    await axios.post("/api/test", body);
+    await axios.post(process.env.BASE_PATH + "/api/test", body);
 
     dispatch({
       type: SEND,
-      payload: { sent: body }
+      payload: { sent: body },
     });
   } catch (e) {
     if (e.response) {
@@ -127,8 +128,8 @@ export const send = ({
           type: ERROR,
           payload: {
             name: errors.namrrors,
-            message: errors.message
-          }
+            message: errors.message,
+          },
         });
       }
     }
@@ -137,8 +138,8 @@ export const send = ({
       type: ERROR,
       payload: {
         name: e.name,
-        message: e.message
-      }
+        message: e.message,
+      },
     });
   }
 };
